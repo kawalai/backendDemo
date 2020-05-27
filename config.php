81cff1a5b05aee939c41acd856a5acf72bf66153
kawalai <kawalai@lee-cheng.com.tw>
2020-05-27 14:04:19 +0800

    init

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php //>
+
+const APP_DATA = APP_HOME . 'data/';
+const APP_LOG = APP_HOME . 'logs/';
+const APP_NAME = 'sample';
+
+const DB_NAME = 'pgsql:dbname=kawalai_demo_db';
+const DB_USER = 'kawalai_demo';
+const DB_PASSWORD = 123;
+
+const DEBUG = true;
